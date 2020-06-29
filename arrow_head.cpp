d0ef688ec35e28c4b76ab4152c8ac3f1312183c7
darkhat1001 <59206738+darkhat1001@users.noreply.github.com>
2020-06-29 19:27:52 +0530

    adding arrow_head code

---
+++ arrow_head.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<"*";
+            }
+        cout<<endl;
+    }
+    for(i=n;i>1;i--)
+    {
+        for(j=1;j<i;j++)
+            cout<<"#";
+        cout<<endl;
+    }
+    return 0;
+}
