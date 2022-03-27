aea4f2301d5182eed8649ce987cf6849377e0dee
Yash Agarwal <59206738+thisisyashagarwal@users.noreply.github.com>
2022-03-27 23:53:11 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (68.60%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -5,10 +5,8 @@ public:
         int m = t.length();
         int i,j = 0;
         for(i=0; i<m; i++)
-        {
             if( t[i] == s[j])
                 j++;
-        }
         if( j == n)
             return 1;
         else
