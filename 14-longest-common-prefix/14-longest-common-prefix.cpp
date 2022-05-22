e35952354ff11e6d8d04eeb781a56958774d4c11
Yash Agarwal <59206738+thisisyashagarwal@users.noreply.github.com>
2022-05-23 01:34:53 +0530

    Time: 8 ms (35.64%), Space: 9.3 MB (45.02%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -17,23 +17,5 @@ public:
                 break; 
         }
         return ans; 
-}
-        /*
-        int n = str.size();
-        int i,j;
-        string s = "";
-        for( j=0; j<min( str[0].length(), str[1].length()); j++)
-        {
-            if( str[0][j] == str[1][j])
-                s += (str[1][j]);
-        }
-        string s1 = "";
-        for( j=0; j<min( s.length(), str[2].length()); j++)
-        {
-            if( str[2][j] == s[j])
-                s1 += (str[2][j]);
-        }
-        
-        return s1;
-    }*/
+    }
 };
