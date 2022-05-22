c0349554f63c9ef39b50f11a4b3ec8eb23f87f17
Yash Agarwal <59206738+thisisyashagarwal@users.noreply.github.com>
2022-05-22 23:08:44 +0530

    Time: 6 ms (24.45%), Space: 6.5 MB (20.45%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string s, string str) {
+        int n = s.length();
+        int m = str.length();
+        string r;
+        for( int i=0; i<n; i++)
+        {
+            r = s.substr( i, m);
+            if( r == str)
+                return i;
+        }
+        return -1;
+    }
+};
