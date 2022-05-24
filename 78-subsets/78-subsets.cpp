3ae9a60f9ea6c2b7e67b63a98cbfbb0a75f347f5
Yash Agarwal <59206738+thisisyashagarwal@users.noreply.github.com>
2022-05-24 16:08:43 +0530

    Time: 0 ms (100.00%), Space: 15.3 MB (26.94%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -4,9 +4,6 @@ public:
     {
         if( idx == n)
         {
-            // for( int i=0; i<v.size(); i++)
-            //     cout<<v[i]<< " ";
-            // cout<<endl;
             v1.push_back(v);
             return;
         }
