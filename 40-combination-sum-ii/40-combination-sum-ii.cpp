03de23fea72d18df5239ad57149affcee6e791d6
Yash Agarwal <59206738+thisisyashagarwal@users.noreply.github.com>
2022-05-28 20:34:33 +0530

    Time: 11 ms (44.60%), Space: 13.3 MB (21.54%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -5,7 +5,6 @@ public:
     {
         if( sum == 0)
         {
-                // sort( v.begin(), v.end());
             v1.push_back(v);
             return;
         }
@@ -17,11 +16,8 @@ public:
             if( (arr[i]) > sum)
                 break;
             v.push_back(arr[i]);
-            // s += arr[i];
             print( i+1, v, arr, n, v1, sum- arr[i]);
-            // s -= arr[i];
             v.pop_back();
-        // print( idx+1, v, arr, n, v1, s, sum);
         }
     }
     
@@ -31,19 +27,7 @@ public:
         vector<int>v;
         vector<vector<int>>v1;
         print( 0, v, candidates, n, v1, target);
-        sort( v1.begin(), v1.end());
-        vector<vector<int>>v2;
-        map<vector<int>, int>m;
-        
-        for( int i=0; i<v1.size(); i++)
-        {
-            if( m.find( v1[i]) == m.end())
-            {
-                v2.push_back(v1[i]);
-                m[v1[i]] = 1;
-            }
-        }
-        
+    
         return v1;
     }
 };
